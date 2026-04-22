fdfa8ef4aa771f0212c06a8b5eafba58cb96397f
Louidporto <135027792+Louidporto@users.noreply.github.com>
2026-04-22 13:58:23 -0300

    Update c54343893.lua

--- script/c54343893.lua
+++ script/c54343893.lua
@@ -1,59 +1,29 @@
---バイス・ドラゴン
---Vice Dragon
 local s,id=GetID()
+
 function s.initial_effect(c)
-	-- Efeito para sempre vir na mão (Adicionado)
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e0:SetCode(EVENT_PREDRAW)
-	e0:SetRange(LOCATION_DECK)
-	e0:SetCondition(s.start_hand_con)
-	e0:SetOperation(s.start_hand_op)
-	c:RegisterEffect(e0)
-
-	--special summon (Original)
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(s.spcon)
-	e1:SetOperation(s.spop)
-	c:RegisterEffect(e1)
+    -- Efeito para garantir que a carta comece na mão
+    -- EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS faz o motor checar o campo/deck constantemente
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e1:SetCode(EVENT_PREDRAW)
+    e1:SetRange(LOCATION_DECK)
+    e1:SetCondition(s.start_hand_con)
+    e1:SetOperation(s.start_hand_op)
+    c:RegisterEffect(e1)
 end
 
--- Funções para o efeito de vir na mão
+-- 1. Condição: Verifica se é o primeiro turno e se a carta ainda está no Deck
 function s.start_hand_con(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnCount()==0
+    return Duel.GetTurnCount()==0
 end
 
+-- 2. Operação: Move a carta para a mão antes mesmo da compra do primeiro turno
 function s.start_hand_op(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsLocation(LOCATION_DECK) then
-		Duel.DisableShuffleCheck()
-		Duel.SendtoHand(c,nil,REASON_RULE)
-	end
-end
-
--- Funções originais do Vice Dragon
-function s.spcon(e,c)
-	if c==nil then return true end
-	return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_MZONE,0,nil)==0
-		and	Duel.GetFieldGroupCount(c:GetControler(),0,LOCATION_MZONE,nil)>0
-		and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
-end
-
-function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_SET_BASE_ATTACK)
-	e1:SetValue(1000)
-	e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE&~RESET_TOFIELD)
-	c:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_SET_BASE_DEFENSE)
-	e2:SetValue(1200)
-	e2:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE-RESET_TOFIELD)
-	c:RegisterEffect(e2)
+    local c=e:GetHandler()
+    if c:IsLocation(LOCATION_DECK) then
+        Duel.DisableShuffleCheck()
+        Duel.SendtoHand(c,nil,REASON_RULE)
+        -- Opcional: Se quiser manter o deck com 5 cartas na mão inicial (padrão)
+        -- você pode adicionar um comando para devolver outra carta aleatória para o deck
+    end
 end
